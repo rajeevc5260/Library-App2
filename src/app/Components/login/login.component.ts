a77785759223b09bc4c377d10faebfebd6f52f99
Rajeev C <81346240+rajeevc5260@users.noreply.github.com>
2022-09-27 19:52:20 +0530

    validation

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -8,6 +8,10 @@ import { SignupComponent } from '../signup/signup.component';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginDetails= {
+    email:'',
+    password:''
+  }
   constructor(private dialogePopUp: MatDialog) {}
   signOpen() {
     this.dialogePopUp.open(SignupComponent)
